64f8f1ba061f0e6bb50e6d7e2aef4b70865f7c9b
Camila Arias <ariasruizcamilaa@gmail.com>
2024-11-05 19:58:57 -0500

    [Fix] - fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Dashboard from './pages/Dashboard';
-// import Posts from './pages/Posts';
+import Dashboard from './pages/Dashboard';
+import Posts from './pages/Posts';
 import Users from './pages/Users';
 import Todos from './pages/Todos';
 import Profile from './pages/Profile';
@@ -14,8 +14,8 @@ function App() {
       <Layout>
         <div className="App">
           <Routes>
-            {/* <Route path="/" element={<Dashboard />} /> */}
-            {/* <Route path="/posts" element={<Posts />} /> */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/posts" element={<Posts />} />
             <Route path="/users" element={<Users />} />
             <Route path="/todos" element={<Todos />} />
             <Route path="/profile" element={<Profile />} />
--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,6 +1,6 @@
 // src/api/services/index.js
-// export * from './postsService';
-// export * from './commentsService';
+export * from './postsService';
+export * from './commentsService';
 export * from './albumsService';
 export * from './photosService';
 export * from './todosService';

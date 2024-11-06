4a5eb42b179b639705e2ead823f8280670e49fb7
Les138 <138824809+Les138@users.noreply.github.com>
2024-11-05 22:33:58 -0500

    Posts y Sidebar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,12 +3,12 @@ import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nex
 export default function NavbarComponent() {
   return (
     <nav className="bg-primary flex justify-between text-white w-screen px-4 py-2 h-[10vh]">
-      <div className="flex justify-center items-center">
-        <img src="/logo.webp" className="w-10 h-10 rounded-full me-4"/>
+      <div className="flex items-center justify-center">
+        <img src="/logo1.png" className="w-10 h-10 rounded-full me-4"/>
         <p className="font-bold text-inherit">infiinicode</p>
       </div>
       <div>
-        <div className="flex justify-center items-center lg:flex hover:bg-customPurple-light1 px-4 py-1 rounded-full cursor-pointer">
+        <div className="flex items-center justify-center px-4 py-1 rounded-full cursor-pointer lg:flex hover:bg-customPurple-light1">
             <h5 className="me-4">
                 @usuario
             </h5>
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const items = [
-    {
-        name: 'Dashboard',
-        icon: 'fas fa-home',
-        href: '/'
-    },
-    {
-        name: 'Posts',
-        icon: 'fas fa-newspaper',
-        href: '/posts'
-    },
-    {
-        name: 'Users',
-        icon: 'fas fa-users',
-        href: '/users'
-    },
+  {
+    name: "Dashboard",
+    icon: "fas fa-home",
+    href: "/",
+  },
+  {
+    name: "Posts",
+    icon: "fas fa-newspaper",
+    href: "/posts",
+  },
+  {
+    name: "Users",
+    icon: "fas fa-users",
+    href: "/users",
+  },
 ];
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col gap-3 bg-primary p-3 rounded-lg m-2 overflow-auto">
-        {items.map((item, index) => (
-            <Link key={index} to={item.href}  className="flex flex-col items-center justify-center text-white border border-white p-4 hover:bg-customPurple-light1 rounded-lg h-20">
-                <i className={item.icon}></i>
-                <span className='mt-2'>{item.name}</span>
-            </Link>
-        ))}
+    <div className="flex flex-col justify-center gap-3 p-3 m-2 overflow-auto rounded-lg bg-primary md:h-auto md:w-60">
+        
+      {items.map((item, index) => (
+        <Link
+          key={index}
+          to={item.href}
+          className="flex flex-col items-center justify-center h-20 p-4 text-white border border-white rounded-lg hover:bg-customPurple-light1 md:h-auto md:flex-col md:gap-2"
+        >
+          <i className={`${item.icon} text-lg`}></i>
+          <span className="hidden mt-2 text-xs md:text-base md:mt-0 md:inline lg:inline-block">
+            {item.name}
+          </span>
+        </Link>
+      ))}
     </div>
   );
 };
 
 export default Sidebar;
+
+
+
